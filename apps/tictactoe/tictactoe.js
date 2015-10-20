8c033b080eccae4b74d40d70757a3e1a7443cde2
gizemny <gizemny@gmail.com>
2015-10-20 18:53:31 -0500

    updated files and added tic tac toe app

---
+++ apps/tictactoe/tictactoe.js
@@ -0,0 +1,40 @@
+'use strict'
+
+$(document).ready(function() {
+  var playerTurn = 'X'; 
+  $('[data-cell]').click(function() {
+    $(this).text(playerTurn);
+    checkForWin();
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+
+  });
+
+function horizontalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn) && ($('[data-cell="1"]').text() === playerTurn) && ($('[data-cell="2"]').text() === playerTurn) ||
+           ($('[data-cell="3"]').text() === playerTurn) && ($('[data-cell="4"]').text() === playerTurn) && ($('[data-cell="5"]').text() === playerTurn) ||
+           ($('[data-cell="6"]').text() === playerTurn) && ($('[data-cell="7"]').text() === playerTurn) && ($('[data-cell="8"]').text() === playerTurn)
+};
+
+function verticalWin() {
+    return ($('[data-cell="0"]').text() === playerTurn) && ($('[data-cell="4"]').text() === playerTurn) && ($('[data-cell="7"]').text() === playerTurn) ||
+           ($('[data-cell="2"]').text() === playerTurn) && ($('[data-cell="5"]').text() === playerTurn) && ($('[data-cell="8"]').text() === playerTurn) ||
+           ($('[data-cell="3"]').text() === playerTurn) && ($('[data-cell="6"]').text() === playerTurn) && ($('[data-cell="9"]').text() === playerTurn)
+};
+
+function diagonalWin() {
+    return ($('[data-cell="1"]').text() === playerTurn) && ($('[data-cell="5"]').text() === playerTurn) && ($('[data-cell="9"]').text() === playerTurn)|| 
+           ($('[data-cell="3"]').text() === playerTurn) && ($('[data-cell="5"]').text() === playerTurn) && ($('[data-cell="7"]').text() === playerTurn) 
+};
+
+
+function checkForWin() {
+  if ( horizontalWin() || verticalWin() || diagonalWin() ) {
+    alert('Player ' +  playerTurn + ' Won!');
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+});
