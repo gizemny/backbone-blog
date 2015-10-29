320f5151328f48dc7d72f7f4878e1ef4fa90ee07
gizemny <gizemny@gmail.com>
2015-10-28 20:19:32 -0500

    finished todo

--- apps/todo/Todo.js
+++ apps/todo/Todo.js
@@ -6,11 +6,20 @@ $(document).ready(function() {
 	 	var entry = $(this).find('input[type="text"]').val();
 	 	$('#todo-list').append(
 	 		'<li class="list-item">'+entry+'</li>'
-	 		);
+	 	);
+	});
+
+
+	$('#todo-list').on('mouseenter','> li', function(event) {
+	   $(this).addClass('done');
+	}).on('mouseleave', '> li', function(event) {
+	   $(this).removeClass('done');
 	});
 
     $('ul').on('click', 'li', function() {
-        $(this).remove();
+		$(this).fadeOut(700, function() {
+        	$(this).remove();
+   		 	});
     });
 
  $('#todo-list').sortable();
