2dc1c72169bfe004ce09f151ede51b92955d536a
gizemny <gizemny@gmail.com>
2015-10-30 20:36:26 -0500

    working on repo

---
+++ apps/repoApp/script.js
@@ -0,0 +1,33 @@
+'use strict';
+$(document).ready(function(){
+	// $.ajax('https://reqres-api.herokuapp.com/api/users', {
+	// 	success: function(users) {
+	// 		_.each(users, function(user) {
+	// 		var str= '<li><a class="person" href="https://reqres-api.herokuapp.com/api/users/' + user['id'] + '">' + user['first_name'] + ' ' + user['last_name']+ '</li>'
+	// 		$('#people').append(str)
+	// 		}); 
+			
+	// 		$('.person').on('click', function(event) {
+	// 			event.preventDefault();
+	// 			$.ajax($(this).attr('href'), {
+	// 				success: function (user) {
+	// 					var str = user['first_name'] + ' ' + user['last_name'];
+	// 					str += '<br><img src="' + user['avatar'] + '">'
+ // 						$('#person-details').html(str);
+	// 				} 
+	// 			});
+	// 		});
+	// 	}
+	// });
+
+	$.ajax('https://api.github.com/users/gizemny/repos', {
+		success: function(repos) {
+			_.each(repos, function(repo) {
+				var str = '<li><a class="project" href="https://api.github.com/repos/gizemny/' + repo['name'] + '/contents">' + '</li>'
+				$('#my-repos').append(str);
+			});
+
+		}
+	});
+
+});
