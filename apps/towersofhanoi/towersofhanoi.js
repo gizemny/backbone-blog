d74ee31a701be125a2d9bccb67398e9193a809c7
gizemny <gizemny@gmail.com>
2015-11-02 20:27:39 -0600

    updated towers game and added some styles to site - wip

--- apps/towersofhanoi/towersofhanoi.js
+++ apps/towersofhanoi/towersofhanoi.js
@@ -1,29 +1,45 @@
+'use strict';
+
 $(document).ready(function() {
 		var $lastChild = null;
 
 	$('[data-stack]').on('click', function () { 
 
-		// console.log( $(this).attr('data-stack') ); get the attribute of the stack
-		if ($lastChild) { // if empty
-			$(this).append($lastChild); // take it and append to data-stack
-			$lastChild = null; // set lastchild to empty again
+		if ($lastChild) { 
+			if( isLegal($lastChild, $(this)) === false){
+				$('#announce-game-won').text("Move Not Allowed");
+				$('#announce-game-won').fadeOut('slow');
+			} else {
+			// if empty
+				$(this).append($lastChild); // append to data-stack
+				if ( checkForWin() === true) {
+					$('announce-game-won').text("You won!");
+					return false;
+				}
+				$lastChild = null; // set lastchild to empty again
+			} 
 		} else { // otherwise
 			$lastChild = $(this).children().last().detach() ; // take the last child and detach it
 		}
-		isLegal();
-		checkForWin();
 	});
-//turn data to 
-	// ParseInt('100') > '50'
-});
 
-function isLegal($stack) {
-	($('[data-stack]').children().last().attr('[data-block]') > $lastChild.attr('[data-block]') || !$stack.children().length === 0);
-}
+
+function isLegal($lastChild, $stack) {
+	if ( parseInt($stack.children().last().attr('data-block')) > parseInt($lastChild.attr('data-block')) || $stack.children().last().attr('data-block') === undefined){	
+		return true;
+		} else {
+		return false;
+		$('#announce-game-won').text("Move Not Allowed");
+		$('#announce-game-won').fadeOut('slow');
+		} 
+	}
+
 function checkForWin() {
-if ($('[data-stack]') === 4) {
-	console.log('Congrats! Game Won!');
-	return true;
-}
-return false;
-}
+	if ( $('[data-stack="2"]').first().children().length === 4 || $('[data-stack="3"]').first().children().length === 4)  {
+		return true;
+		} else { 
+		return false;
+		}
+	}
+
+});
