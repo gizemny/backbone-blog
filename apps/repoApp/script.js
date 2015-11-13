15b2845df8fc79eec195aa1d52eb365535fc6a72
gizemny <gizemny@gmail.com>
2015-11-12 18:34:20 -0600

    finished repoApp

--- apps/repoApp/script.js
+++ apps/repoApp/script.js
@@ -1,35 +1,22 @@
 'use strict';
 $(document).ready(function(){
-	// $.ajax('https://reqres-api.herokuapp.com/api/users', {
-	// 	success: function(users) {
-	// 		_.each(users, function(user) {
-	// 		var str= '<li><a class="person" href="https://reqres-api.herokuapp.com/api/users/' + user['id'] + '">' + user['first_name'] + ' ' + user['last_name']+ '</li>'
-	// 		$('#people').append(str)
-	// 		}); 
-			
-	// 		$('.person').on('click', function(event) {
-	// 			event.preventDefault();
-	// 			$.ajax($(this).attr('href'), {
-	// 				success: function (user) {
-	// 					var str = user['first_name'] + ' ' + user['last_name'];
-	// 					str += '<br><img src="' + user['avatar'] + '">'
- // 						$('#person-details').html(str);
-	// 				} 
-	// 			});
-	// 		});
-	// 	}
-	// });
-
 	$.ajax('https://api.github.com/users/gizemny/repos', {
 		success: function(repos) {
 			_.each(repos, function(repo) {
-				var str = '<li><a class="project" href="https://api.github.com/repos/gizemny/' + repo['name'] + '/contents">' + '</li>'
+				var str = '<li><a class="project" href="https://api.github.com/repos/gizemny/' + repo['name'] + '/contents">' + repo['name'] + '</a></li>'
 				$('#my-repos').append(str);
 			});
 
-		$('project')
-
+			$('project').on('click', function(event) {
+				event.preventDefault();
+				$.ajax($(this).attr('href'), {
+					success: function (repo) {
+						var str = repo['name'];
+						str += '<li><a class="project" href="' + repo['url'] + '"></li>'
+						$('#person-details').html(str);
+					} 
+				});
+			});
 		}
 	});
-
 });
